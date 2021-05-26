6fd47820e33cc4e613729352e577c9da752e9d95
zhangyp <zyp19930326>
2021-05-26 09:17:12 +0800

    feat: 支持跨域

--- src/main/java/cc/mrbird/febs/FebsApplication.java
+++ src/main/java/cc/mrbird/febs/FebsApplication.java
@@ -3,6 +3,7 @@ package cc.mrbird.febs;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("cc.mrbird.febs.*.mapper")
+@ServletComponentScan
 public class FebsApplication {
 
     public static void main(String[] args) {
