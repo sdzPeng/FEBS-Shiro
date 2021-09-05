cbcc3bbba6a35a498663228e9c8c4965fed72c74
zhangyp <zyp19930326>
2021-09-05 11:37:55 +0800

    fix: 修改控制台打印

--- src/main/resources/static/febs/index.js
+++ src/main/resources/static/febs/index.js
@@ -3,6 +3,6 @@ layui.extend({
     validate: 'lay/modules/validate'
 }).define(['febs', 'conf'], function (exports) {
     layui.febs.initPage();
-    console.log("\n %c FEBS-Shiro 2.0 %c https://github.com/wuyouzhuguli/FEBS-Shiro %c 如果该项目对您有帮助的话，还请点个star给予精神支持！🐤", "color: #fff; font-size: .84rem;background: #366ed8; padding:5px 0;", "font-size: .84rem;background: #fff; border: 2px solid #b0e0a8;border-left: none; padding:3px 0;"," font-size: .84rem;background: #fcf9ec; padding:5px 0;margin-left: 8px");
+    console.log("\n hello word 🐤", "color: #fff; font-size: .84rem;background: #366ed8; padding:5px 0;", "font-size: .84rem;background: #fff; border: 2px solid #b0e0a8;border-left: none; padding:3px 0;"," font-size: .84rem;background: #fcf9ec; padding:5px 0;margin-left: 8px");
     exports('index', {});
 });
